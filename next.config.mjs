49d8f5b805ffe693bb2c0dd7950e79bb09a99b83
kbyeffah <effahboakye357@gmail.com>
2025-03-29 01:24:57 +0000

    update for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'ucarecdn.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'wordpress-1327581-4857058.cloudwaysapps.com',
-        },
-      ],
-    },
-  }
-  
-  export default nextConfig
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ucarecdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wordpress-1327581-4857058.cloudwaysapps.com',
+      },
+    ],
+  },
+  experimental: {
+    runtime: 'nodejs', // Force Node.js runtime
+  },
+};
+
+export default nextConfig;
